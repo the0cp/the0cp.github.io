913fdca3f071ce680a3b492f81e79dc82ee91157
Theodore Cooper <63190431+theodorecooper@users.noreply.github.com>
2020-11-01 09:49:37 +0800

    Site updated: 2020-11-01 09:49:36

--- js/js.js
+++ js/js.js
@@ -1,14 +1,4 @@
 var web_style = $("#web_style").val();
-var valine_appid = $("#valine_appid").val();
-var valine_appKey = $("#valine_appKey").val();
-
-new Valine({
-    el: '#vcomments',
-    appId: valine_appid,
-    appKey: valine_appKey,
-    placeholder: 'input...',
-    avatar: "wavatar"
-})
 
 document.addEventListener('DOMContentLoaded', (event) => {
     document.querySelectorAll('pre').forEach((block) => {

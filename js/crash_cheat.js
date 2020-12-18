74fcad6e5e27867b70111f0f5353e61e92d5cdc5
Theodore Cooper <63190431+theodorecooper@users.noreply.github.com>
2020-12-18 21:11:26 +0800

    Site updated: 2020-12-18 21:11:13

--- js/crash_cheat.js
+++ js/crash_cheat.js
@@ -1,13 +1,1 @@
-var OriginTitle = document.title
-var titleTime
-document.addEventListener('visibilitychange', function() {
-  if (document.hidden) {
-    document.title = 'Page crashed!!!'
-    clearTimeout(titleTime)
-  } else {
-    document.title = 'Bazinga!!!'
-    titleTime = setTimeout(function() {
-      document.title = OriginTitle
-    }, 2000)
-  }
-})
+var titleTime,OriginTitle=document.title;document.addEventListener("visibilitychange",(function(){document.hidden?(document.title="Page crashed!!!",clearTimeout(titleTime)):(document.title="Bazinga!!!",titleTime=setTimeout((function(){document.title=OriginTitle}),2e3))}));

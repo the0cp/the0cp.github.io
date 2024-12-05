c991f9e3db2e61f33794914ecc86c9632de27c40
Theodore Cooper <ccooperr2005@gmail.com>
2024-12-05 21:50:21 +0800

    Add tags

--- assets/js/data/swconf.js
+++ assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1733406387',resources: [
+    cacheName: 'chirpy-1733406585',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       

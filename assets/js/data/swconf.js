9f67e90c48bd34e657e8c7a160cbc4998f3abbea
Theodore Cooper <ccooperr2005@gmail.com>
2025-01-04 20:42:42 +0800

    fix violations

--- assets/js/data/swconf.js
+++ assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1735822837',resources: [
+    cacheName: 'chirpy-1735994491',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       

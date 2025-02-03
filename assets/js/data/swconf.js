5ef4ee044dbc1c879fb625bab23c4256ae1bbd3d
Theodore Cooper <ccooperr2005@gmail.com>
2025-02-03 19:26:20 +0800

    add yandex

--- assets/js/data/swconf.js
+++ assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1737994967',resources: [
+    cacheName: 'chirpy-1738581923',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       

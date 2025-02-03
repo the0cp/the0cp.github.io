8e866492f8ce7ffbd28e120f56eeb01f399bfbbd
Theodore Cooper <ccooperr2005@gmail.com>
2025-02-03 19:43:04 +0800

    add IndexNow

--- assets/js/data/swconf.js
+++ assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1738581923',resources: [
+    cacheName: 'chirpy-1738582933',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       

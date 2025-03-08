a8b42a28a8d9ec60a77205606d1f8467b8b6ef01
Theodore Cooper <ccooperr2005@gmail.com>
2025-03-08 12:55:59 +0800

    add kmp demo

--- assets/js/data/swconf.js
+++ assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1740561942',resources: [
+    cacheName: 'chirpy-1741409714',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       

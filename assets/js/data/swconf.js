319d49eef4f9f849baa49a500a9c36da6b7e6702
Theodore Cooper <ccooperr2005@gmail.com>
2025-03-16 17:03:15 +0800

    update url

--- assets/js/data/swconf.js
+++ assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1741409714',resources: [
+    cacheName: 'chirpy-1742115746',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       

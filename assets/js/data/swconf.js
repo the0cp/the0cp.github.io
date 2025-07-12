6381aa263db47c78e7c1771f0915018ad16d10a9
Theodore Cooper <ccooperr2005@gmail.com>
2025-07-12 18:50:35 +0800

    add d-tsk-fc article

--- assets/js/data/swconf.js
+++ assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1752152371',resources: [
+    cacheName: 'chirpy-1752317388',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       

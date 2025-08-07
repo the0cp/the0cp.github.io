f625d7ca65e7d0af287f702b50d246d6dc1668f1
Theodore Cooper <ccooperr2005@gmail.com>
2025-08-07 23:54:43 +0800

    change avatar

--- assets/js/data/swconf.js
+++ assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1752393665',resources: [
+    cacheName: 'chirpy-1754582024',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       

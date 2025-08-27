ae1430e050c691df1465afc87578d294bda98bb4
Theodore Cooper <ccooperr2005@gmail.com>
2025-08-27 20:25:33 +0800

    add cookieyes

--- assets/js/data/swconf.js
+++ assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1755416383',resources: [
+    cacheName: 'chirpy-1756297478',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       

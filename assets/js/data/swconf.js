2589ddca48f25a234c87556a18dbc11143a361b1
Theodore Cooper <ccooperr2005@gmail.com>
2026-03-30 17:54:20 +0800

    add article: lang-p5

--- assets/js/data/swconf.js
+++ assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1774687196',resources: [
+    cacheName: 'chirpy-1774864407',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       

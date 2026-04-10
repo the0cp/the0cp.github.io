6f0f759ee8c21d4a9caac585610bc86a3ec64667
Theodore Cooper <ccooperr2005@gmail.com>
2026-04-10 11:07:28 +0800

    add article, update cover

--- assets/js/data/swconf.js
+++ assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1775560180',resources: [
+    cacheName: 'chirpy-1775790369',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       

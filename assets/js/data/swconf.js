4764e3de5f3102dc2016dbbdc738033cd6d7f032
Theodore Cooper <ccooperr2005@gmail.com>
2026-04-29 18:06:33 +0800

    pin

--- assets/js/data/swconf.js
+++ assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1777456866',resources: [
+    cacheName: 'chirpy-1777457136',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       

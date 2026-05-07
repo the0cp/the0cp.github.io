7c5efecc84651f22f7a7d4ee0e2a1be0fe99c000
Theodore Cooper <ccooperr2005@gmail.com>
2026-05-07 22:16:53 +0800

    add article: lang-p12

--- assets/js/data/swconf.js
+++ assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1777457136',resources: [
+    cacheName: 'chirpy-1778163364',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
